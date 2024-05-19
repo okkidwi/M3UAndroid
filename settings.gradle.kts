048865220876149eff0ed61dbe560ea797b0de9e
oxy <sortby@qq.com>
2024-05-19 22:11:34 +0800

    build: add mirror maven repositories for Chinese mainland.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
         google()
         mavenCentral()
         maven("https://plugins.gradle.org/m2/")
+
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
     }
 }
 dependencyResolutionManagement {
@@ -12,7 +16,9 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://jitpack.io")
         maven("https://plugins.gradle.org/m2/")
-        maven("https://androidx.dev/snapshots/builds/11670047/artifacts/repository/")
+
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/google")
     }
 }
 rootProject.name = "M3U"
@@ -33,4 +39,3 @@ include(":benchmark")
 include(":i18n")
 include(":ui")
 include(":codec:lite", ":codec:rich")
-include(":features:playlist-configuration")
